a1de5119afb323a0abef998a62ea4ba564434836
viralthaker15 <viralthaker27@gmail.com>
2020-09-06 19:28:57 +0530

    organised styles & cleaned data

--- src/components/CovidApp.js
+++ src/components/CovidApp.js
@@ -1,7 +1,12 @@
 import React from "react";
 import axios from "axios";
+import stateCodes from "../Static/stateCodes";
+import months from "../Static/months";
+// ===== Styles =====
+import { withStyles } from "@material-ui/core";
+import styles from "../styles/CovidApp";
 
-export default class CovidApp extends React.Component {
+class CovidApp extends React.Component {
 	constructor(props) {
 		super(props);
 
@@ -10,8 +15,11 @@ export default class CovidApp extends React.Component {
 			todayData: {},
 			isLoading: false,
 			mapData: [],
-			tableData: [],
 		};
+
+		this.fetchData = this.fetchData.bind(this);
+		this.findStateId = this.findStateId.bind(this);
+		this.getMapData = this.getMapData(this);
 	}
 
 	componentDidMount() {
@@ -40,18 +48,71 @@ export default class CovidApp extends React.Component {
 				//extracting necessary data from extracted data
 				const [todayData] = countryData.statewise.slice(0, 1);
 				const casesTimeline = countryData.cases_time_series; //timeline summary of cases
-				console.log(casesTimeline);
+				const data = countryData.statewise.slice(1, -1); //last two data are irrelevent and gives 0 data
+
+				this.setState(
+					{
+						data: data,
+						todayData: todayData,
+						casesTimeline: casesTimeline,
+						districtLevel: districtLevel,
+						updates: updates,
+						expanded: false,
+					},
+					this.getMapData
+				);
 			})
 		);
 	}
 
-	state = {};
+	//formatting data
+	formatData(data) {
+		const formatData = data.map(singleData => {
+			return {
+				id: this.findStateId(singleData.state), // state codes gujarat -> GJ
+				state: singleData.state.replace(" and ", " & "),
+				value: singleData.confirmed,
+			};
+		});
+
+		return formatData;
+	}
+
+	findStateId(state) {
+		for (const [key, value] of Object.entries(stateCodes))
+			if (key === state) return value;
+	}
+
+	getMapData() {
+		const newData = this.formatData(this.state.data);
+
+		this.setState({
+			mapData: newData,
+		});
+
+		//this.state.mapData = newData;
+	}
+
+	formatDate(date) {
+		try {
+			const day = date.slice(0, 2);
+			const month = date.slice(3, 5);
+			const time = date.slice(11);
+			return `${day} ${months[month]}, ${time.slice(0, 5)} IST`;
+		} catch (err) {
+			console.log(err);
+		}
+	}
 
 	render() {
+		const { mapData, data, districtLevel, expanded, updates } = this.state;
+
 		return (
-			<div>
+			<div className={this.props.classes.header}>
 				<h1>CovidApp</h1>
 			</div>
 		);
 	}
 }
+
+export default withStyles(styles)(CovidApp);
